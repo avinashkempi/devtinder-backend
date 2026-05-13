82858eb15621f1a31425d9d1a61a42b036e04a8f
Avinash Kempi <avinash.kempi@gmail.com>
2026-05-13 16:08:00 +0530

    message changes

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,9 +4,11 @@ const User = require("../models/user");
 const userAuth = async (req, res, next) => {
   try {
     const { access_token } = req.cookies;
-    if (!access_token) throw new Error("Please Login");
+    if (!access_token) {
+      res.status(401).send("Please Login");
+    }
     const decoded = await jwt.verify(access_token, "DevTinder@$123");
-    user = await User.findById(decoded._id);
+    const user = await User.findById(decoded._id);
     if (!user) throw new Error("User Not Found");
     req.user = user;
     next();
